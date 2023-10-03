ad07b290c9e93e917d0cb124ceb75dda099fbb0c
Chester Lee Coloma <cleecoloma@gmail.com>
2023-10-03 14:10:29 -0700

    added interval 5 seconds for 5 orders with help from chatGPT

--- clients/vendor/index.js
+++ clients/vendor/index.js
@@ -6,7 +6,22 @@ const io = require('socket.io-client');
 const capsSocket = io.connect(SERVER_URL + '/caps');
 const { createPickup, handleDelivery } = require('./handler.js');
 
+// capsSocket.emit('pickup', createPickup('1-206-flowers'));
 
-capsSocket.emit('pickup', createPickup('1-206-flowers'));
+let intervalCount = 0;
+const maxIntervals = 5; // Number of intervals you want to run
+
+// Define the function to emit 'pickup' event
+function emitPickupEvent() {
+  capsSocket.emit('pickup', createPickup('1-206-flowers'));
+  intervalCount++;
+
+  if (intervalCount === maxIntervals) {
+    clearInterval(intervalId); // Stop the setInterval after 5 intervals
+  }
+}
+
+// Start the setInterval
+const intervalId = setInterval(emitPickupEvent, 5000); // 5000 milliseconds = 5 seconds
 
 capsSocket.on('delivered', handleDelivery);
